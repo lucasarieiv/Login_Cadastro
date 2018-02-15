b42d0deb51723fbdec8e685ea172216db0687468
Lucas Vieira <tiomedianope@gmail.com>
2018-02-15 16:53:44 -0200

    Validando Senha Para Login

--- Login.py
+++ Login.py
@@ -8,22 +8,28 @@ def resPass():
     resp = input() #Essa Linha ta uma entrada
     return resp
 
-
 while True:
     resp = resPass()
     if resp == '1':
         print('Bem Vindo Ao Nosso Programa')
-        print('Digite Seu Login:')
-        login = input()
-        print('Digite Sua Senha')
-        userPass = input()
-        for key, value in user.items():
-            if login == key and userPass == value:
-                print('Logado Com Sucesso')
-                break
-            else:
-                print('Você Não Se Encontra Cadastrado no Banco de Dados')
-                resp = resPass()
+        login = input('Digite Seu Login: ')
+        for u in os.listdir('Cadastros\\'):
+            if (login.upper() + '.txt') == u:
+                senha = input('Digite Sua Senha: ')
+                uLog = open('Cadastros\\' + u, 'r')
+                cLog = uLog.read()
+                cont = 0
+                for ul in cLog.split('\n'):
+                    cont += 1
+                    if cont == 3:
+                        if ul == senha.upper():
+                            print('Bem Vindo')
+                        elif ul != senha.upper():
+                            while ul != senha.upper():
+                                print('Senha Incorreta!')
+                                senha = input('Digite Sua Senha Novamente: ')
+                            print('Bem Vindo!')
+                uLog.close()
     elif resp == '2':
         cadastroPessoas()
 
